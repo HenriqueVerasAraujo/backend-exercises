8a2701756060dda901c0443d92e011e745aa5e7a
Henrique_Veras <theveras1@gmail.com>
2022-07-20 19:40:53 -0300

    Exercicio pizza feito

--- pratica-typescript-2/dist/index.js
+++ pratica-typescript-2/dist/index.js
@@ -1,52 +1,20 @@
 "use strict";
-var Color;
-(function (Color) {
-    Color["black"] = "black";
-    Color["red"] = "red";
-    Color["blue"] = "blue";
-})(Color || (Color = {}));
+// enum Color {
+//     black = 'black',
+//     red = 'red',
+//     blue = 'blue'
+// };
 ;
-var Door;
-(function (Door) {
-    Door["backdoor"] = "back doors";
-    Door["frontdoor"] = "front doors";
-})(Door || (Door = {}));
-;
-var Turn;
-(function (Turn) {
-    Turn["right"] = "The car has turned right";
-    Turn["left"] = "The car has turned left";
-})(Turn || (Turn = {}));
-;
-class Car {
-    constructor(brand, color, doors) {
-        this.brand = brand;
-        this.color = color;
-        this.doors = doors;
-    }
-    mySpecs() {
-        return [this.brand, this.color, this.doors];
-    }
-    honk() {
-        console.log('honk honk');
+class Pizza {
+    constructor(flavor, slices) {
+        this.flavor = flavor;
+        this.slices = slices;
     }
     ;
-    openTheDoor(door) {
-        console.log(`The ${door} opened`);
-    }
-    ;
-    closeTheDoor(door) {
-        console.log(`The ${door} closed`);
-    }
-    ;
-    turnOn() {
-        return 'vrum vrum';
-    }
-    turn(command) {
-        return console.log(command);
+    myPizza() {
+        console.log(`${this.flavor}, ${this.slices} fatias`);
     }
 }
-const myCar = new Car('Fusca', Color.black, 4);
-console.log(myCar.mySpecs());
-myCar.turn(Turn.right);
-myCar.openTheDoor(Door.backdoor);
+;
+const pizza1 = new Pizza('Calabresa', 4);
+pizza1.myPizza();
--- pratica-typescript-2/index.ts
+++ pratica-typescript-2/index.ts
@@ -1,58 +1,84 @@
-enum Color {
-    black = 'black',
-    red = 'red',
-    blue = 'blue'
+// enum Color {
+//     black = 'black',
+//     red = 'red',
+//     blue = 'blue'
+// };
+
+// enum Door {
+//     backdoor = 'back doors',
+//     frontdoor = 'front doors',
+// };
+
+// enum Turn {
+//     right = 'The car has turned right',
+//     left = 'The car has turned left'
+// };
+
+// interface Car {
+//     brand: string,
+//     color: Color,
+//     doors: number,
+//     honk(): void,
+//     openTheDoor(door: Door): void,
+//     closeTheDoor(door: Door): void,
+//     turnOn(): string
+//     turn(command: Turn): void,
+//     mySpecs(): string[],
+// }
+// class Car {
+//     constructor(brand: string, color: Color, doors: number) {
+//         this.brand = brand;
+//         this.color = color;
+//         this.doors = doors;
+//     }
+//     mySpecs() {
+//         return [this.brand, this.color, this.doors];
+//     }
+//     honk() {
+//         console.log('honk honk');
+//     } ;
+//     openTheDoor(door: Door) {
+//         console.log(`The ${door} opened`);
+//     };
+//     closeTheDoor(door: Door) {
+//         console.log(`The ${door} closed`);
+//     };
+//     turnOn() {
+//         return 'vrum vrum'
+//     }
+//     turn(command: Turn) {
+//         return console.log(command);
+//     }
+// }
+
+// const myCar = new Car('Fusca', Color.black, 4);
+
+// console.log(myCar.mySpecs());
+// myCar.turn(Turn.right);
+// myCar.openTheDoor(Door.backdoor);
+type Slices = 4 | 6 | 8;
+type SlicesSweet = 4;
+
+interface Pizza {
+    flavor: string,
+    slices: Slices,
 };
 
-enum Door {
-    backdoor = 'back doors',
-    frontdoor = 'front doors',
-};
-
-enum Turn {
-    right = 'The car has turned right',
-    left = 'The car has turned left'
-};
-
-interface Car {
-    brand: string,
-    color: Color,
-    doors: number,
-    honk(): void,
-    openTheDoor(door: Door): void,
-    closeTheDoor(door: Door): void,
-    turnOn(): string
-    turn(command: Turn): void,
-    mySpecs(): string[],
-}
-class Car {
-    constructor(brand: string, color: Color, doors: number) {
-        this.brand = brand;
-        this.color = color;
-        this.doors = doors;
-    }
-    mySpecs() {
-        return [this.brand, this.color, this.doors];
-    }
-    honk() {
-        console.log('honk honk');
-    } ;
-    openTheDoor(door: Door) {
-        console.log(`The ${door} opened`);
+class Pizza {
+    constructor(flavor: string, slices: Slices) {
+        this.flavor = flavor;
+        this.slices = slices;
     };
-    closeTheDoor(door: Door) {
-        console.log(`The ${door} closed`);
-    };
-    turnOn() {
-        return 'vrum vrum'
+    myPizza():void {
+        console.log(`${this.flavor}, ${this.slices} fatias`);
     }
-    turn(command: Turn) {
-        return console.log(command);
-    }
+};
+
+const pizza1 = new Pizza('Calabresa', 8);
+const pizza2 = new Pizza('Marguerita', 6);
+const pizza3 = new Pizza('Nutella', 4);
+
+interface standardPizza extends Pizza {
+
 }
-
-const myCar = new Car('Fusca', Color.black, 4);
-
-console.log(myCar.mySpecs());
-myCar.turn(Turn.right);
-myCar.openTheDoor(Door.backdoor);
+pizza1.myPizza();
